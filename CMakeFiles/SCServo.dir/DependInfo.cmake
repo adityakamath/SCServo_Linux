d4779307d0c3b71a0aa3f2748e7dd2f204e7ad64
Aditya Kamath <adityakamath@live.com>
2024-08-14 19:08:58 +0200

    add build files

--- CMakeFiles/SCServo.dir/DependInfo.cmake
+++ CMakeFiles/SCServo.dir/DependInfo.cmake
@@ -8,16 +8,20 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/ubuntu/ros2_ws/src/pan_tilt_ros/include/SCServo_Linux/SCS.cpp" "CMakeFiles/SCServo.dir/SCS.cpp.o" "gcc" "CMakeFiles/SCServo.dir/SCS.cpp.o.d"
-  "/home/ubuntu/ros2_ws/src/pan_tilt_ros/include/SCServo_Linux/SCSCL.cpp" "CMakeFiles/SCServo.dir/SCSCL.cpp.o" "gcc" "CMakeFiles/SCServo.dir/SCSCL.cpp.o.d"
-  "/home/ubuntu/ros2_ws/src/pan_tilt_ros/include/SCServo_Linux/SCSerial.cpp" "CMakeFiles/SCServo.dir/SCSerial.cpp.o" "gcc" "CMakeFiles/SCServo.dir/SCSerial.cpp.o.d"
-  "/home/ubuntu/ros2_ws/src/pan_tilt_ros/include/SCServo_Linux/SMSBL.cpp" "CMakeFiles/SCServo.dir/SMSBL.cpp.o" "gcc" "CMakeFiles/SCServo.dir/SMSBL.cpp.o.d"
-  "/home/ubuntu/ros2_ws/src/pan_tilt_ros/include/SCServo_Linux/SMSCL.cpp" "CMakeFiles/SCServo.dir/SMSCL.cpp.o" "gcc" "CMakeFiles/SCServo.dir/SMSCL.cpp.o.d"
-  "/home/ubuntu/ros2_ws/src/pan_tilt_ros/include/SCServo_Linux/SMS_STS.cpp" "CMakeFiles/SCServo.dir/SMS_STS.cpp.o" "gcc" "CMakeFiles/SCServo.dir/SMS_STS.cpp.o.d"
+  "/Users/adityakamath/Documents/ros2pixi/ros2h_project/src/pan_tilt_ros/include/SCServo_Linux/SCS.cpp" "CMakeFiles/SCServo.dir/SCS.cpp.o" "gcc" "CMakeFiles/SCServo.dir/SCS.cpp.o.d"
+  "/Users/adityakamath/Documents/ros2pixi/ros2h_project/src/pan_tilt_ros/include/SCServo_Linux/SCSCL.cpp" "CMakeFiles/SCServo.dir/SCSCL.cpp.o" "gcc" "CMakeFiles/SCServo.dir/SCSCL.cpp.o.d"
+  "/Users/adityakamath/Documents/ros2pixi/ros2h_project/src/pan_tilt_ros/include/SCServo_Linux/SCSerial.cpp" "CMakeFiles/SCServo.dir/SCSerial.cpp.o" "gcc" "CMakeFiles/SCServo.dir/SCSerial.cpp.o.d"
+  "/Users/adityakamath/Documents/ros2pixi/ros2h_project/src/pan_tilt_ros/include/SCServo_Linux/SMSBL.cpp" "CMakeFiles/SCServo.dir/SMSBL.cpp.o" "gcc" "CMakeFiles/SCServo.dir/SMSBL.cpp.o.d"
+  "/Users/adityakamath/Documents/ros2pixi/ros2h_project/src/pan_tilt_ros/include/SCServo_Linux/SMSCL.cpp" "CMakeFiles/SCServo.dir/SMSCL.cpp.o" "gcc" "CMakeFiles/SCServo.dir/SMSCL.cpp.o.d"
+  "/Users/adityakamath/Documents/ros2pixi/ros2h_project/src/pan_tilt_ros/include/SCServo_Linux/SMS_STS.cpp" "CMakeFiles/SCServo.dir/SMS_STS.cpp.o" "gcc" "CMakeFiles/SCServo.dir/SMS_STS.cpp.o.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/ubuntu/ros2_ws/src/pan_tilt_ros/include/SCServo_Linux
+# Install script for directory: /Users/adityakamath/Documents/ros2pixi/ros2h_project/src/pan_tilt_ros/include/SCServo_Linux
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -27,28 +27,31 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
-# Install shared libraries without execute permission?
-if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "1")
-endif()
-
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-# Set default install directory permissions.
+# Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+  set(CMAKE_OBJDUMP "/Library/Developer/CommandLineTools/usr/bin/objdump")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
-  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+  if(CMAKE_INSTALL_COMPONENT MATCHES "^[a-zA-Z0-9_.+-]+$")
+    set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+  else()
+    string(MD5 CMAKE_INST_COMP_HASH "${CMAKE_INSTALL_COMPONENT}")
+    set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INST_COMP_HASH}.txt")
+    unset(CMAKE_INST_COMP_HASH)
+  endif()
 else()
   set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
 endif()
 
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/ubuntu/ros2_ws/src/pan_tilt_ros/include/SCServo_Linux/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/Users/adityakamath/Documents/ros2pixi/ros2h_project/src/pan_tilt_ros/include/SCServo_Linux/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
+endif()
